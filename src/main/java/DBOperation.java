9726457229885ace743d39348ed5a12a68d9f556
alvinseptiano <spazmantiz.dev@gmail.com>
2021-12-29 17:51:16 +0700

    add database operation

--- src/main/java/DBOperation.java
+++ src/main/java/DBOperation.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author alvin
- */
+
 public class DBOperation {
     
 }
