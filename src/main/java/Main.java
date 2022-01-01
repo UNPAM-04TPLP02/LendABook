357cf0c33567230c2ac6c248735f29e65ebc8edf
alvinseptiano <spazmantiz.dev@gmail.com>
2022-01-01 14:21:51 +0700

    Change variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,20 +39,6 @@ public class Main extends javax.swing.JFrame {
         );
     }// </editor-fold>//GEN-END:initComponents
     
-    public static class Listener implements ActionListener { 
-        
-        public void actionPerformed(Component e) { 
-            e.getComponentListeners();
-            String objname = e.getName();
-            System.out.println(objname);
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    }
-    
     public static void main(String args[]) {
         FlatDarkLaf.setup(); //Dark Theme
         // Inisialisasi komponen
@@ -60,11 +46,6 @@ public class Main extends javax.swing.JFrame {
         LoginUI login = new LoginUI();
         PinjamUI pinjam = new PinjamUI();
         EditUI editui = new EditUI();
-        
-        Listener loginListener = new Listener();
-        Listener pinjamListener = new Listener();
-        Listener edituiListener = new Listener();
-        pinjamListener.actionPerformed(pinjam);
 
         frame.setContentPane(login);
         frame.pack();
@@ -74,9 +55,9 @@ public class Main extends javax.swing.JFrame {
         frame.setExtendedState(Main.MAXIMIZED_BOTH); // Buat seluruh program fullscreen
         
         java.awt.EventQueue.invokeLater(() -> {
-            login.setVisible(true);
-            login.addComponentListener((ComponentListener) frame);
-            // frame.setVisible(true);  
+            //login.setVisible(true);
+            //login.addComponentListener((ComponentListener) frame);
+            frame.setVisible(true);  
         });
             
     }
